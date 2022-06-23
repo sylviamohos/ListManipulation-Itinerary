f680882013a8b0df5257b97160b0069fea208473
SylviaMohos <sylvia.mohos@gmail.com>
2022-06-22 21:32:29 -0700

    Create project to showcase simple List manipulation

---
+++ com/amazon/ata/lists/Itinerary.java
@@ -0,0 +1,90 @@
+package com.amazon.ata.lists;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Class representing travel itinerary. The basic functionality it supports is to add, get or remove
+ * a destination. Additionally, it allows a user to get a list of locations that are going to be visited
+ * and get the total number of days that will be spent in trip.
+ */
+public class Itinerary {
+
+    private List<Destination> destinations;
+
+    public Itinerary() {
+         this.destinations = new ArrayList<>();
+    }
+
+
+    /**
+     * Add a new destination to the end of itinerary.
+     *
+     * @param destination destination to add to itinerary
+     */
+    public void addDestination(Destination destination) {
+        // Implement the method here
+        destinations.add(destination);
+    }
+
+    /**
+     * Get the destination from itinerary based on its position.
+     *
+     * @param position position of destination to retrieve.
+     * @return Destination at position.
+     */
+    public Destination getDestination(int position) {
+        return destinations.get(position);
+    }
+
+    /**
+     * Remove a destination from itinerary based on its position.
+     *
+     * @param position position of destination to remove.
+     * @return destination that was removed from itinerary.
+     */
+    public Destination removeDestination(int position) {
+        return destinations.remove(position);
+    }
+
+
+    /**
+     * Get list of locations (cities) in the order in which they
+     * appear in the itinerary.
+     *
+     * @return full list of locations from itinerary.
+     */
+    public List<String> getListOfLocations() {
+        // Implement the method here
+        List<String> strList = new ArrayList<>();
+        for (Destination d : destinations) {
+            strList.add(d.getLocation());
+        }
+        return strList;
+    }
+
+    /**
+     * Get total number of days spent on the trip planned by the
+     * itinerary.
+     *
+     * @return total number of days spent in all locations.
+     */
+    public int getTotalNumberOfDays() {
+        // Implement the method here
+        int totalDays = 0;
+        for (Destination d : destinations) {
+            totalDays += d.getDaysAtLocation();
+        }
+        return totalDays;
+    }
+
+    /**
+     * Get number of destinations contained in itinerary.
+     *
+     * @return number of destinations contained in itinerary.
+     */
+    public int getNumberOfDestinations() {
+        // Implement the method here
+        return destinations.size();
+    }
+}
